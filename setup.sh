99c14510956e59dd6368163d10fceb54263cbe75
Zeenathul Nizreen <nizreenzeenathul@gmail.com>
2024-05-01 15:35:53 +0000

    updated setup

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ pip install -r requirements.txt
 # Install TRL from a specific GitHub commit
 pip install git+https://github.com/huggingface/trl@a3c5b7178ac4f65569975efadc97db2f3749c65e --upgrade
 
-# Install flash-attn with control over the number of build jobs
-pip install ninja packaging
-MAX_JOBS=2 pip install flash-attn --no-build-isolation
 
+import torch; assert torch.cuda.get_device_capability()[0] >= 8, 'Hardware not supported for Flash Attention'
+# install flash-attn
+!pip install ninja packaging
+!MAX_JOBS=2 pip install flash-attn --no-build-isolation
